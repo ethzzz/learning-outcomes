cb241fb0d79fb7cfa2167d8350b9c0c849b3729b
Ethz <zenghao@whalehouse.com>
2021-04-24 13:14:36 +0800

    添加node.js axios依赖

--- Node.js/mysql.js
+++ Node.js/mysql.js
@@ -1,20 +1,29 @@
 // 引入mysql模块
 let mysql = require('mysql')
+// 引入http模块
+let http = require('http')
 
 // 建立一个数据库连接
 let connection = mysql.createConnection({
     host: 'localhost',
-    dbname: 'test',
+    database: 'sys',
     user: 'root',
-    password: '123',
+    password: '123456',
     port: '3306'
 })
+
+// 建立一个http服务
+let httpServer = http.createServer()
+
+
 
 // 连接数据库
 connection.connect()
 
 // 执行mysql语句
 connection.query('show tables', function (error, result, fields) {
-    if (error) throw error
+    if (error) {
+        throw error
+    }
     console.log(result)
 })
