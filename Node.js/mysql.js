c6f427e2a270e3522972959e21f109bf84deb71d
ethzzz <1098620570@qq.com>
2024-02-19 17:52:04 +0800

    change log

--- Node.js/mysql.js
+++ Node.js/mysql.js
@@ -1,29 +1,32 @@
 // 引入mysql模块
 let mysql = require('mysql')
-// 引入http模块
-let http = require('http')
 
 // 建立一个数据库连接
 let connection = mysql.createConnection({
     host: 'localhost',
-    database: 'sys',
+    database: 'testdb',
     user: 'root',
     password: '123456',
     port: '3306'
 })
 
-// 建立一个http服务
-let httpServer = http.createServer()
-
-
-
 // 连接数据库
 connection.connect()
 
-// 执行mysql语句
-connection.query('show tables', function (error, result, fields) {
-    if (error) {
-        throw error
+connection.query(`update user set password = '122112' where username = 'test'`, (err, res) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(res)
     }
-    console.log(result)
 })
+
+function exec_sql(sql, callback) {
+    connection.query(sql, callback)
+}
+
+exec_sql(`select * from user`, (err, res) => {
+    console.log(res);
+})
+
+module.exports = connection
